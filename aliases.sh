fa4bc80ccb1848cde7aaf03b1e086af758ff1bd2
Davide Dippolito <davide.dippolito@onthebeach.co.uk>
2013-09-28 02:57:13 +0100

    Organization of dotfiles

---
+++ aliases.sh
@@ -0,0 +1,15 @@
+alias gitup=set_github_remote
+
+alias ...='cd ../..'
+alias ll='ls -alF'
+
+alias be='bundle exec'
+alias bi='bundle install'
+alias rc='rails console'
+
+alias gs='git status'
+alias e='vim'
+
+alias restart='sudo service nginx restart'
+alias s=.\ ~/dotfiles/functions.sh
+alias ack='ack-grep --type-set haml=.haml --type-set eruby=.erb'
---
+++ functions.sh
@@ -0,0 +1,17 @@
+set_github_remote() { git remote set-url origin git@github.otbeach.com:onthebeach/"$1".git; }
+
+translate_branch_name() { echo "$1" | tr / \;; }
+
+git_root() { git rev-parse --show-toplevel; }
+
+git_current_branch() { git branch | grep "*" | cut -c3-; }
+
+app_name() { git_root | sed -e 's/\/current$//' -e 's/.*\///'; }
+
+string_with_dots() { echo "$1...$2"; }
+
+branches() { string_with_dots $(translate_branch_name $1) $(translate_branch_name $2); }
+
+compare_branches() { google-chrome $(compare_url ${1-master} ${2-$(git_current_branch)}); }
+
+compare_url() { echo "https://github.otbeach.com/onthebeach/$(app_name)/compare/$(branches $1 $2)"; }
